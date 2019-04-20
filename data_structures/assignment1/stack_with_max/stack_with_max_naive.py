d408eae7bd94160bca66a06524df0ab377c22cbf
Aditya Sahai <adityaatgatech@gmail.com>
2019-04-20 06:50:55 -0700

    added interview kickstart

--- data_structures/assignment1/stack_with_max/stack_with_max_naive.py
+++ data_structures/assignment1/stack_with_max/stack_with_max_naive.py
@@ -4,17 +4,26 @@ import sys
 class StackWithMax():
     def __init__(self):
         self.__stack = []
+        self.__max = []
 
     def Push(self, a):
         self.__stack.append(a)
+        if self.__max:
+            if self.__max[-1] > a:
+                self.__max.append(self.__max[-1])
+            else:
+                self.__max.append(a)
+        else:
+            self.__max.append(a)
 
     def Pop(self):
         assert(len(self.__stack))
         self.__stack.pop()
+        self.__max.pop()
 
     def Max(self):
         assert(len(self.__stack))
-        return max(self.__stack)
+        return self.__max[-1]
 
 
 if __name__ == '__main__':
---
+++ interview_kickstart/heap.py
@@ -0,0 +1,56 @@
+'''
+Heap class
+'''
+
+class Heap(object):
+	def __init__(self, arr):
+		self.arr = arr
+		self.size = len(arr)
+		self.build_heap()
+
+	def build_heap(self):
+		for i in range(self.size//2 - 1, -1, -1):
+			self.heapify_down(i)
+
+	def heapify_down(self, idx):
+		to_swap = idx
+		left_child = self.get_left(idx)
+		right_child = self.get_right(idx)
+
+		if left_child and self.arr[left_child] < self.arr[to_swap]:
+			to_swap = left_child
+
+		if right_child and self.arr[right_child] < self.arr[to_swap]:
+			to_swap = right_child
+
+		if to_swap != idx:
+			self.swap(to_swap, idx)
+			self.heapify_down(to_swap)
+
+	def get_left(self, idx):
+		left_child = 2 * idx + 1
+		return left_child if left_child < self.size else None
+
+	def get_right(self, idx):
+		right_child = 2 * idx + 2
+		return right_child if right_child < self.size else None
+
+	def swap(self, i, j):
+		self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
+
+	def heap_sort(self):
+		n = self.size
+
+		while self.size > 0:
+			self.swap(0, self.size-1)
+			self.size -= 1
+			self.heapify_down(0)
+
+		self.size = n
+		return list(reversed(self.arr))
+
+
+if __name__ == '__main__':
+	arr = [1,23,123,123223,0,-123,-1423534,2]
+	h = Heap(arr[:])
+	assert h.heap_sort() == sorted(arr)
